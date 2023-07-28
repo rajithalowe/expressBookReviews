72c982438d13293f48953430de4ce6c767d8dedd
rajithalowe <l_rajitha@hotmail.com>
2023-07-28 03:16:15 -0400

    updated version

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,57 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    let userFound = users.filter((user)=>{
+        return user.username === username;
+    });
+    if(userFound.length > 0){
+        return false;    
+    }
+    else{
+        return true;
+    }    
+    
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    let validUser = users.filter((user)=>{
+        return user.username === username && user.password === password;
+    });
+    if(validUser.length > 0){
+        return true;
+    }
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username  = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password){
+        return res.status(404).json({message:"Error login in"});
+    }
+
+    if(authenticatedUser(username,password)){
+        let accessToken = jwt.sign({data:password},'access',{expiresIn:60*60});
+
+        req.session.authorization = {
+            accessToken , username
+        }
+
+        return res.status(200).json({message:"User successfully logged in"})
+    }
+    else{
+        return res.status(404).json({message:"Login error. please check the username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   const isbn = req.params.isbn;
+   const review = req.query.review;
+   const username = req.session.authorization['username'];
 });
 
 module.exports.authenticated = regd_users;
